3da0b8a9617e816eb77d6c1870795fb3e1151005
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-15 01:33:47 +0530

    Time: 3 ms (91.21%), Space: 10.6 MB (16.26%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -21,25 +21,36 @@ public:
         //     }
         // }
         // return ans;
-        vector<int> ans;
-        int n=nums1.size();
-        int m=nums2.size();
-        sort(nums1.begin(),nums1.end()); //1,1,2,2
-        sort(nums2.begin(),nums2.end());//2,2
-        int i=0,j=0;
-        while(i<n && j<m)
+        // vector<int> ans;
+        // int n=nums1.size();
+        // int m=nums2.size();
+        // sort(nums1.begin(),nums1.end()); //1,1,2,2
+        // sort(nums2.begin(),nums2.end());//2,2
+        // int i=0,j=0;
+        // while(i<n && j<m)
+        // {
+        //     if(nums1[i]==nums2[j])
+        //     {
+        //         if(ans.empty() || ans.back()!=nums1[i])
+        //             ans.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j])
+        //         i++;
+        //     else
+        //         j++;
+        // }
+        // return ans;
+        unordered_set<int>s;
+        unordered_set<int>s_one;
+        s.insert(begin(nums1),end(nums1));
+        s_one.insert(begin(nums2),end(nums2));
+        vector<int>ans;
+        for(int i:s_one)
         {
-            if(nums1[i]==nums2[j])
-            {
-                if(ans.empty() || ans.back()!=nums1[i])
-                    ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-                i++;
-            else
-                j++;
+            if(s.count(i)>0)
+                ans.push_back(i);
         }
         return ans;
     }    
