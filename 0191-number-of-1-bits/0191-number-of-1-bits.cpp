167e73f4fc22992519c108bc901ea8994094b8be
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-15 01:10:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (70.98%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -8,12 +8,17 @@ public:
         //     n/=2;    
         // }
         // return count;
-        int count=0;
-        while(n)
-        {
-            count+=n&1;
-            n>>=1;
-        }
-        return count;
+        // int count=0;
+        // while(n)
+        // {
+        //     count+=n&1;
+        //     n>>=1;
+        // }
+        // return count;
+        //string s=to_string(n);
+        
+        string s = bitset<32>(n).to_string(); // Convert the unsigned integer to a binary string
+        bitset<32>bit_string(s);
+        return bit_string.count();
     }
 };
