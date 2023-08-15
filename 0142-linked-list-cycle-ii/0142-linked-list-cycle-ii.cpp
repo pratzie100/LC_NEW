e65d0cf9c28dc29fa43f94d31e4c80bf4d0e19ae
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-15 22:05:36 +0530

    0142-linked-list-cycle-ii.cpp

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,7 +9,9 @@
 class Solution {
 public:
     //Modified Floyd Cycle Detection Algorithm:
-    //once the fast and slow pointers meet inside the cycle, we reset one pointer back to the head and then move both pointers at the same pace until they meet again. The meeting point is the starting node of the cycle. This approach ensures that the returned node is the starting node of the cycle
+    //once the fast and slow pointers meet inside the cycle, we reset one pointer back to the head 
+//and then move both pointers at the same pace until they meet again. The meeting point is the starting node of
+//the cycle. This approach ensures that the returned node is the starting node of the cycle
     ListNode *detectCycle(ListNode *head) {
         if(!head || !head->next) return NULL; //if one or no nodes
         ListNode* slow=head;
