7fd4d3133b39f6776a9858a1949136c1ffcdb351
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-15 12:13:37 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (81.87%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void solve(int ind,vector<vector<int>>&ans,vector<int>&nums)
-    {
-        if(ind==nums.size())
-        {
-            ans.push_back(nums); 
-            return;   
-        }
-        for(int i=ind;i<nums.size();i++)
-        {
-            swap(nums[ind],nums[i]);
-            solve(ind+1,ans,nums);
-            swap(nums[ind],nums[i]);
-        }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        solve(0,ans,nums);
-        return ans;              
-    }
-    // void solve(vector<int>&ds,vector<vector<int>>&ans,vector<bool>&visited,vector<int>&nums)
+    
+    // void solve(int ind,vector<vector<int>>&ans,vector<int>&nums)
+    // {
+    //     if(ind==nums.size())
+    //     {
+    //         ans.push_back(nums); 
+    //         return;   
+    //     }
+    //     for(int i=ind;i<nums.size();i++)
+    //     {
+    //         swap(nums[ind],nums[i]);
+    //         solve(ind+1,ans,nums);
+    //         swap(nums[ind],nums[i]); //backtrack
+    //     }
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>>ans;
+    //     solve(0,ans,nums);
+    //     return ans;              
+    // }
+    
+    //void solve(vector<int>&ds,vector<vector<int>>&ans,vector<bool>&visited,vector<int>&nums)
     // {
     //     if(ds.size()==nums.size())
     //     {
@@ -45,4 +47,13 @@ public:
     //     solve(ds,ans,visited,nums);
     //     return ans;              
     // }
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(begin(nums),end(nums));
+        vector<vector<int>>ans;
+        do
+        {
+            ans.push_back(nums);
+        } while(next_permutation(begin(nums),end(nums)));
+        return ans;
+    }
 };
