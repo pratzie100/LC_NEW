f176a003243d6e1f53ffc6fddd46fb115bad6d21
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-15 22:16:21 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (26.83%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,13 +9,13 @@
 class Solution {
 public:
     //Modified Floyd Cycle Detection Algorithm:
-    //once the fast and slow pointers meet inside the cycle, we reset one pointer back to the head 
-//and then move both pointers at the same pace until they meet again. The meeting point is the starting node of
-//the cycle. This approach ensures that the returned node is the starting node of the cycle
+    //once the fast and slow pointers meet inside the cycle, we reset one pointer back to the head and then move both pointers at the same pace until they meet again. The meeting point is the starting node of the cycle. This approach ensures that the returned node is the starting node of the cycle
     ListNode *detectCycle(ListNode *head) {
         if(!head || !head->next) return NULL; //if one or no nodes
         ListNode* slow=head;
-        ListNode* fast=head;
+        ListNode* fast=head;//To find the starting node of the cycle, you want both pointers to start at the same position so that you can measure the exact length of the cycle.
+// Once you know the length, you can use a second pointer to move ahead by that length, while keeping the first pointer at the head. When they meet again, you have found the starting node of the cycle.
+        
         while(true)
         {
             if(!fast || !fast->next) return NULL; // No cycle
