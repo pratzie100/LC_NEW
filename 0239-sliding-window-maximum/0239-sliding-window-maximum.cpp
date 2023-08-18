176bd449923f3bdb0a12e9e8f122fafcf8d0b9b3
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-19 00:18:51 +0530

    Time: 520 ms (13.28%), Space: 175.5 MB (18.06%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int>maxSlidingWindow(vector<int>& nums, int k) {
+        map<int,int>m;
         vector<int>ans;
-        deque<int>dq;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            while(!dq.empty() && dq.back()<nums[i])
+            if(i<k-1)
             {
-                dq.pop_back();
+                m[nums[i]]++;
+                continue;
             }
-            if(!dq.empty() && i>=k && dq.front()==nums[i-k])
+            else
             {
-                dq.pop_front();
-            }
-            dq.push_back(nums[i]);
-            if(i>=k-1)
-            {
-                ans.push_back(dq.front());
+                m[nums[i]]++;
+                auto it=m.rbegin();
+                ans.push_back(it->first);
+                m[nums[i-k+1]]--;
+                if(m[nums[i-k+1]]==0)
+                {
+                    m.erase(nums[i-k+1]);
+                }
             }
         }
         return ans;
-
     }
+    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    //     vector<int>ans;
+    //     deque<int>dq;
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         while(!dq.empty() && dq.back()<nums[i]) 
+    //         {
+    //             dq.pop_back();
+    //         }
+    //         if(!dq.empty() && i>=k && dq.front()==nums[i-k])
+    //         {
+    //             dq.pop_front();
+    //         }
+    //         dq.push_back(nums[i]);
+    //         if(i>=k-1)
+    //         {
+    //             ans.push_back(dq.front());
+    //         }
+    //     }
+    //     return ans;
+
+    // }
 };
