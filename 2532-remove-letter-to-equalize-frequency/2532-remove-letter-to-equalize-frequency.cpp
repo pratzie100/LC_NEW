aa6550c70a2d22ccaed6db99edc688c1cf0a874c
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-19 01:09:02 +0530

    Time: 3 ms (41.65%), Space: 7.1 MB (7.8%) - LeetHub

---
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool check(int i,string str)
+    {
+        str.erase(i,1);
+        unordered_map<int,int>m;
+        for(int i=0;i<str.size();i++)
+        {
+            m[str[i]]++;
+        }
+        for(int i=1;i<str.size();i++)
+        {
+            if(m[str[i]]!=m[str[i-1]]) return false;
+        }
+        return true;
+    }
+    bool equalFrequency(string word) {
+        for(int i=0;i<word.size();i++)
+        {
+            if(check(i,word))
+                return true;
+        }
+        return false;
+    }
+};
