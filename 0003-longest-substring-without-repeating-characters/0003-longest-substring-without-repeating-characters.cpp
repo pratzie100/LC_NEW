745df3a63b2162dde9462398faca026daf5d1fba
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-19 19:25:28 +0530

    Time: 10 ms (80.98%), Space: 8.4 MB (64.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //abca //abcababcdc
+        //abca //abcababcdc //acdebfbabcdc //acbdefbabcdc //abba
         unordered_map<char,int>m;
-        int l=0,r=0,res=0;
-        for(int r=0;r<s.size();r++)
+        int left=0,right=0,res=0;
+        for(int right=0;right<s.size();right++)
         {
-            char curr=s[r];
-            if(m[curr]>0) //if s[r] already exist in map
-                l=max(l,m[curr]);
-            m[curr]=r+1;  // update current s[r]'s value
-            res=max(res,m[curr]-l);  //m[s[r]]-l is current substring
+            if(m[s[right]]>0) //if s[right] already exist in map
+                left=max(left,m[s[right]]); //update left pointer //here we will not do left=m[s[right]] as left should always move forward..there will be chance that s[right] previously occured way before where left is present currently,in that case left=m[s[right]] will shift left pointer backwards leading to wrong current window size calculation at res(3rd line) ..ex- abba 
+            m[s[right]]=right+1;  // update length of max substring ending at right pointer
+            res=max(res,m[s[right]]-left);  //m[s[right]]-left is current substring window size..ex //acdebfbabcdc when right reaches second 'b' ,then 
+            //res=max(6,7-5)=6 //note at this line res already has length of max required substring before current iteration
         }
         return res;
+
     }
 };
