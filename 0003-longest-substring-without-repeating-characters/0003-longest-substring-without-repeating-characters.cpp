437c3ebe96d87c409280d25172d9e8cc670f897b
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-19 19:27:40 +0530

    Update 0003-longest-substring-without-repeating-characters.cpp

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,8 +7,12 @@ public:
         for(int right=0;right<s.size();right++)
         {
             if(m[s[right]]>0) //if s[right] already exist in map
-                left=max(left,m[s[right]]); //update left pointer //here we will not do left=m[s[right]] as left should always move forward..there will be chance that s[right] previously occured way before where left is present currently,in that case left=m[s[right]] will shift left pointer backwards leading to wrong current window size calculation at res(3rd line) ..ex- abba 
+                left=max(left,m[s[right]]); //update left pointer //here we will not do left=m[s[right]] as left should always move forward..there will
+            //be chance that s[right] previously occured way before where left is present currently,in that case left=m[s[right]] will shift left 
+            //pointer backwards leading to wrong current window size calculation at res(3rd line) ..ex- abba 
+            
             m[s[right]]=right+1;  // update length of max substring ending at right pointer
+            
             res=max(res,m[s[right]]-left);  //m[s[right]]-left is current substring window size..ex //acdebfbabcdc when right reaches second 'b' ,then 
             //res=max(6,7-5)=6 //note at this line res already has length of max required substring before current iteration
         }
