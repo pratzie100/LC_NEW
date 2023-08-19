e602063f17b9ba518b9b07eb313d08e3068a9f60
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-19 13:22:18 +0530

    Time: 115 ms (95.87%), Space: 77.2 MB (76.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& histo) {
-        stack<int>st;
+        stack<int>st; //stack stores indices of the bars in ascending order of heights.
         int maxA = 0;
         int n=histo.size();
+        //For each histogram bar, we compare its height with the height of the bar at the top of the stack.
+// If the current bar's height is greater than or equal to the height of the bar at the top of the stack, we simply push the current index onto the stack.
+// If the current bar's height is smaller than the height of the bar at the top of the stack, it means we can calculate the area of rectangles that end at the current index. We pop the index from the stack and calculate the width as the difference between the current index and the index at the top of the stack. The height of the rectangle is the height of the bar that was at the top of the stack. We continue this process until we find a bar with a smaller height or the stack becomes empty.
+// After processing all bars, there might be remaining bars in the stack. These bars can be used to calculate the area of rectangles that extend until the end of the histogram.
+// The maximum area encountered during this process is the area of the largest rectangle in the histogram.
         for(int i = 0; i <= n; i++) 
         {
-            while(!st.empty() && (i==n || histo[st.top()] >= histo[i])) 
+            while(!st.empty() && (i==n || histo[i] < histo[st.top()])) 
             {
                 int height = histo[st.top()];
-                
                 st.pop();
                 int width;
                 if(st.empty())
@@ -17,7 +21,6 @@ public:
                 else
                     width = i - st.top() - 1;
                 maxA = max(maxA, width * height);
-                cout<<maxA<<endl;
             }
             st.push(i);
         }
