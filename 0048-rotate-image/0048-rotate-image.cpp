9e2b20e2bbc06e49f36620cef0f574b71edcd315
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-25 13:03:57 +0530

    Time: 4 ms (36.08%), Space: 7.2 MB (16.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) { 
-        int n=mat.size();
+
         //reverse matrix rows
         reverse(mat.begin(),mat.end());
         
         //7 8 9
         //4 5 6
         //1 2 3
-        
-        //transpose the matrix
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-                swap(mat[i][j],mat[j][i]);
-        }
 
-        //i=0 
-        // mat[0][0] /mat[0][0]
-        // mat[0][1] /mat[1][0]
-        // mat[0][2] /mat[2][0]
-        //i=1
-        // mat[1][1] /mat[1][1]
-        // mat[1][2] /mat[2][1]
-        //i=2
-        // mat[2][2] /mat[2][2]
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                ans[j][i]=mat[i][j];
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                mat[i][j]=ans[i][j];
+            }
+        }
+        
+        // //transpose the matrix
+        // int m=mat.size();
+        // int n=mat[0].size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=i;j<n;j++)
+        //         swap(mat[i][j],mat[j][i]);
+        // }
+
+        // //i=0 
+        // // mat[0][0] /mat[0][0]
+        // // mat[0][1] /mat[1][0]
+        // // mat[0][2] /mat[2][0]
+        // //i=1
+        // // mat[1][1] /mat[1][1]
+        // // mat[1][2] /mat[2][1]
+        // //i=2
+        // // mat[2][2] /mat[2][2]
     }
 };
