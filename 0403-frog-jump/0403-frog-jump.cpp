0ea761fe40eec08d4db1c196436060d1e1499da4
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-27 09:56:45 +0530

    Time: 24 ms (99.64%), Space: 210.4 MB (31.72%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-vector<vector<bool>> dp;
-
-bool helper(const vector<int>& stones, int lastIndex, int currentIndex) {
-    if (currentIndex == stones.size() - 1) {
-        return true;
-    }
-
-    if (dp[lastIndex][currentIndex]) {
+    bool solve(int lastIndex, int currentIndex,vector<vector<int>>&dp,vector<int>&stones) {
+        if(currentIndex == stones.size()-1) return true;
+        if(dp[lastIndex][currentIndex]) return false;
+        
+        int lastJump = stones[currentIndex] - stones[lastIndex];
+        int nextIndex = currentIndex + 1;
+        while(nextIndex < stones.size() && stones[nextIndex] <= stones[currentIndex] + lastJump + 1) 
+        {
+            int nextJump = stones[nextIndex] - stones[currentIndex];
+            int jump = nextJump - lastJump;
+            if (jump >= -1 && jump <= 1) 
+            {
+                if(solve(currentIndex, nextIndex,dp,stones)) 
+                return true;       
+            }
+            nextIndex++;
+        }
+        dp[lastIndex][currentIndex] = true;
         return false;
     }
-
-    int lastJump = stones[currentIndex] - stones[lastIndex];
-
-    int nextIndex = currentIndex + 1;
-
-    while (nextIndex < stones.size() && stones[nextIndex] <= stones[currentIndex] + lastJump + 1) {
-        int nextJump = stones[nextIndex] - stones[currentIndex];
-        int jump = nextJump - lastJump;
-
-        if (jump >= -1 && jump <= 1) {
-            if (helper(stones, currentIndex, nextIndex)) {
-                return true;
-            }
-        }
-        nextIndex++;
-    }
-
-    dp[lastIndex][currentIndex] = true;
-
-    return false;
-}
-
     bool canCross(vector<int>& stones) {
-        if(stones[1] != 1)  return false;
-        int n = stones.size();
-        dp = vector<vector<bool>>(n, vector<bool>(n, false));   
-        return helper(stones, 0, 1);
+        if(stones[1]!=1)  return false;
+        int n=stones.size();
+        vector<vector<int>>dp(n,vector<int>(n,false));
+        return solve(0,1,dp,stones);
     }
 };
