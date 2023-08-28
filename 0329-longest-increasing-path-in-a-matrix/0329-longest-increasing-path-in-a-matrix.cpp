a8c04bd513a24ec9906a7ff00c4497955a2a5204
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-28 22:54:29 +0530

    Time: 36 ms (79.00%), Space: 16.1 MB (42.69%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> moves{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    int dfs(vector<vector<int>>& matrix, vector<vector<int>>& dp, int row, int col) {
-        if(dp[row][col] != -1) return dp[row][col];
-        
+    int dfs(int i, int j, vector<vector<int>>& dp,vector<vector<int>>& mat) {
+        if(dp[i][j] != -1) return dp[i][j];
+
         int maxPath = 1;
         for(auto& move : moves) 
         {
-            int newRow = row + move[0];
-            int newCol = col + move[1];
-            if(newRow>=0 && newRow<matrix.size() && newCol>=0 && newCol< matrix[0].size() && matrix[newRow][newCol] > matrix[row][col]) 
+            int newi = i + move[0];
+            int newj = j + move[1];
+            if(newi>=0 && newi<mat.size() && newj>=0 && newj<mat[0].size()&& mat[newi][newj] > mat[i][j]) 
             {
-                maxPath = max(maxPath, 1 + dfs(matrix, dp, newRow, newCol));
+                maxPath = max(maxPath, 1 + dfs(newi,newj,dp,mat));
             }
         }
-        return dp[row][col] = maxPath;
+        return dp[i][j] = maxPath;
     }
 
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return 0;
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+    int longestIncreasingPath(vector<vector<int>>& mat) {
+        if (mat.empty()) return 0;
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>>dp(m, vector<int>(n, -1));
         int maxLength = 0;
-        for(int i=0; i<m; i++) 
+        for(int i=0;i<m; i++) 
         {
             for(int j=0;j<n;j++) 
             {
-                maxLength = max(maxLength, dfs(matrix, dp, i, j));
+                maxLength = max(maxLength, dfs(i,j,dp,mat));
             }
         }
         return maxLength;
