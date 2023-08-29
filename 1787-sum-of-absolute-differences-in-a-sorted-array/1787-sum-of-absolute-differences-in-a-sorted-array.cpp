3340f1a35159442bbecfc79042dc434198ef2fe1
Kargeti_P <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-30 01:58:39 +0530

    Time: 90 ms (78.56%), Space: 87.9 MB (20.36%) - LeetHub

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        //bruteforce->tle
+        int n=nums.size();
+        vector<int>ans(n);
+        vector<int>ps(n+1,0);
+        vector<int>ss(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            ps[i]=ps[i-1]+nums[i-1];
+        }
+        // for(int i:ps) cout<<i<<" ";
+        // cout<<endl;
+        for(int i=1;i<=n;i++)
+        {
+            ss[i]=ss[i-1]+nums[n-i];
+        }
+        // for(int i:ss) cout<<i<<" ";
+        for(int i=0;i<n;i++)
+        {
+            int leftsum=(nums[i]*i)-ps[i];
+            int rightsum=ss[n-i-1]-(nums[i]*(n-i-1));
+            ans[i]=leftsum+rightsum;
+        }
+        return ans;
+    }   
+
+};
