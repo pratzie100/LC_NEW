0945184c4edd8cc1f5c39b1f90ccbb10752bc430
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-01 08:18:28 +0530

    Time: 3 ms (89.82%), Space: 7.8 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -19,20 +19,20 @@ public:
         // return ans;
 
         //Method 2
-        vector<int> ans;
-        for(int i=0;i<=n;i++)
-            ans.push_back(__builtin_popcount(i));
-        return ans;
+        // vector<int> ans;
+        // for(int i=0;i<=n;i++)
+        //     ans.push_back(__builtin_popcount(i));
+        // return ans;
 
         //Method 3
-        // vector<int> dp(n+1, 0);
-        // dp[0]=0;
-        // //no. of ones = no. of ones without lsb(right shift) + lsb
-        // for(int i = 1; i<=n; i++)
-        // {
-        //     dp[i] = dp[i/2] + i%2;
-        // }
-        // return dp;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        //no. of ones = no. of ones without lsb(right shift) + lsb
+        for(int i = 1; i<=n; i++)
+        {
+            dp[i] = dp[i/2] + i%2;
+        }
+         return dp;
 
     }
 };
