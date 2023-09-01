d04219924b916c647cfbcf164c4680a4340b610a
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-01 08:04:44 +0530

    Time: 3 ms (89.82%), Space: 8.5 MB (14.67%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         //Method 1
-        vector<int> ans;
-        for(int i=0;i<=n;i++)
-        {
-            int sum=0;
-            int num=i;
-            while(num>0)
-            {
-                // sum+=num%2;
-                // num=num/2;
-                sum+=num&1;
-                num>>=1;
-            }
-            ans.push_back(sum);
-        }
-        return ans;
-
-        //Method 2
         // vector<int> ans;
         // for(int i=0;i<=n;i++)
-        //     ans.push_back(__builtin_popcount(i));
+        // {
+        //     int sum=0;
+        //     int num=i;
+        //     while(num>0)
+        //     {
+        //         // sum+=num%2;
+        //         // num=num/2;
+        //         sum+=num&1;
+        //         num>>=1;
+        //     }
+        //     ans.push_back(sum);
+        // }
         // return ans;
+
+        //Method 2
+        vector<int> ans;
+        for(int i=0;i<=n;i++)
+            ans.push_back(__builtin_popcount(i));
+        return ans;
 
         //Method 3
         // vector<int> dp(n+1, 0);
