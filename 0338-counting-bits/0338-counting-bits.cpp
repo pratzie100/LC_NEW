fef871b76f58dc6918cc9b1dc78e010a8bffadcd
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-01 08:04:29 +0530

    Time: 9 ms (25.59%), Space: 8.4 MB (32.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         //Method 1
-        // vector<int> ans;
-        // for(int i=0;i<=n;i++)
-        // {
-        //     int sum=0;
-        //     int n=i;
-        //     while(n>0)
-        //     {
-        //         sum+=n%2;
-        //         n=n/2;
-        //     }
-        // }
-        // return ans;
+        vector<int> ans;
+        for(int i=0;i<=n;i++)
+        {
+            int sum=0;
+            int num=i;
+            while(num>0)
+            {
+                // sum+=num%2;
+                // num=num/2;
+                sum+=num&1;
+                num>>=1;
+            }
+            ans.push_back(sum);
+        }
+        return ans;
 
         //Method 2
         // vector<int> ans;
@@ -22,14 +25,14 @@ public:
         // return ans;
 
         //Method 3
-        vector<int> dp(n+1, 0);
-        dp[0]=0;
-        //no. of ones = no. of ones without lsb(right shift) + lsb
-        for(int i = 1; i<=n; i++)
-        {
-            dp[i] = dp[i/2] + i%2;
-        }
-        return dp;
+        // vector<int> dp(n+1, 0);
+        // dp[0]=0;
+        // //no. of ones = no. of ones without lsb(right shift) + lsb
+        // for(int i = 1; i<=n; i++)
+        // {
+        //     dp[i] = dp[i/2] + i%2;
+        // }
+        // return dp;
 
     }
 };
