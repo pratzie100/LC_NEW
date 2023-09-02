50dad3cfcf292969a05e54a30a38b4fab0a39f90
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-02 21:54:43 +0530

    Time: 7 ms (20.00%), Space: 6.3 MB (60.00%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        unordered_map<char,int>m;
+        for(int i=0;i<s1.size();i+=2)
+        {
+            m[s1[i]]++;
+            m[s2[i]]--;
+        }
+        for(auto i:m)
+        {
+            if(i.second!=0) return false;
+        }
+        for(int j=1;j<s1.size();j+=2)
+        {
+            m[s1[j]]++;
+            m[s2[j]]--;
+        }
+        for(auto i:m)
+        {
+            if(i.second!=0) return false;
+        }
+        return true;
+
+    }
+};
