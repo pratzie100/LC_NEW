ec2fc99f2241149a93828abb779502b61b5ac007
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-02 14:54:03 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (46.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>>ans;
-        vector<int>temp;
         for(int row=1;row<=n;row++)
         {
-            temp.clear();
-            int a=1;
-            for(int i=1;i<=row;i++)
+            vector<int>temp;
+            int elem=1;//first value in current row, which is always 1 in Pascal's Triangle.
+            for(int col=1;col<=row;col++)
             {
-                temp.push_back(a);
-                a=a*(row-i)/i;
+                temp.push_back(elem);
+                elem=elem*(row-col);
+                elem/=col;
             }
             ans.push_back(temp);
         }
