13eac1d40eb9c3d37009d6b6dbb95c8f4874568d
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-03 06:17:04 +0530

    Time: 4 ms (14.96%), Space: 6.6 MB (6.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&dp,int m,int n,int i,int j)
+    int solve(int i,int j,vector<vector<int>>&dp,int m,int n)
     {
-        if(i==m-1&&j==n-1)
-            return 1;
-        if(i>=m||j>=n)
-             return 0;
-        if(dp[i][j]!=-1)
+        if(i==m-1&&j==n-1) 
+            return 1; //found one path
+        
+        if(i>=m||j>=n) // since only down and right movement is allowed
+             return 0; //didnt find correct path , return 0
+        
+        if(dp[i][j]!=-1) //if already computed
             return dp[i][j];
-        return dp[i][j]=solve(dp,m,n,i+1,j)+solve(dp,m,n,i,j+1);
+        
+        int down=solve(i+1,j,dp,m,n);
+        int right=solve(i,j+1,dp,m,n);
+        return dp[i][j]=down+right;
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        int x=solve(dp,m,n,0,0);
-        return x;
+        return solve(0,0,dp,m,n);
     }
 };
