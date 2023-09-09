34825377126de53bdbd2473e8af29faf166a6e94
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-09 15:53:28 +0530

    Time: 3 ms (50.37%), Space: 6.6 MB (85.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int combinationSum4(std::vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        dp[0]=1;
+        return solve(dp,target,nums);
+    }
+
+    int solve(vector<int>& dp,int target,vector<int>&nums) {
+        if(dp[target]!=-1) return dp[target];
+        int ans = 0;
+        for (int i=0; i<nums.size(); i++) 
+        {
+            if (target - nums[i] >= 0) 
+                ans+=solve(dp,target-nums[i],nums);
+        }
+        dp[target]=ans;
+        return ans;
+    }
+};
