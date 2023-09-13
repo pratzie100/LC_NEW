8bb610dc95474d07d1939c0ebed1b372d0dace49
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-13 09:47:46 +0530

    Time: 18 ms (53.51%), Space: 18.1 MB (24.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int candy(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> l(n, 1);
-        vector<int> r(n, 1);
-        
-        for (int i = 1; i < n; i++)
-            if (nums[i] > nums[i - 1]) l[i] = l[i - 1] + 1;
-        for (int i = n - 2; i>=0; i--)
-            if (nums[i] > nums[i + 1]) r[i] = r[i + 1] + 1;
-        
-        
-        int res = 0;
-        for (int i =0;i<n;i++)
+        int n=nums.size();
+        vector<int>l(n,1); 
+        vector<int>r(n,1);
+        for(int i=1;i<n;i++)
         {
-            res+= max(l[i], r[i]);
+            if(nums[i]>nums[i-1]) //compare with left child
+            {
+                l[i]=l[i-1]+1;
+            }
         }
-        return res;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]>nums[i+1])  //compare with right child
+            {
+                r[i]=r[i+1]+1;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=max(l[i],r[i]);
+        }
+        return ans;
     }
 };
