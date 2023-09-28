d44befaadef58a37e78a577713b5378c2c0e620e
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-28 09:41:39 +0530

    Time: 3 ms (96.18%), Space: 16.5 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]%2==0)
-            {
-                ans.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]%2!=0)
-            {
-                ans.push_back(nums[i]);
-            }
-        }
+        vector<int>ans(nums);
+        sort(begin(ans),end(ans),[&](int &a,int &b){
+            if(a%2==0 && b%2!=0) return true; 
+            if(a%2!=0 && b%2==0) return false; 
+            return a<b;
+        });
         return ans;
         
+        
+        // vector<int> ans;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]%2==0)
+        //     {
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]%2!=0)
+        //     {
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+        // return ans;
     }
 };
