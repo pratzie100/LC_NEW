732add851a7356b087118ca92d5f40393ddce12d
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-09-29 07:50:44 +0530

    Time: 419 ms (5.16%), Space: 166.6 MB (5.31%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        int pattern=0;
-        for(int i=0;i<nums.size()-1;i++)
+        bool ans1=true,ans2=true;
+        multiset<int>s;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]<nums[i+1]) //if monotone increasing
-            {
-                if(pattern==0)
-                    pattern=1; //set increasing status
-                else if(pattern==-1) // if found decreasing status in btw
-                    return false; 
-            }
-            else if(nums[i]>nums[i+1]) //if monotone decreasing
-            {
-                if(pattern==0)
-                    pattern=-1; //set decreasing status
-                else if(pattern==1) //if found increasing status in btw
-                    return false;
-            }
-        } //nums[i]==nums[i+1] case is always true
-        return true;
+            s.insert(nums[i]);
+            auto it1=s.rbegin();
+            if(*it1!=nums[i]) ans1=false;
+            auto it2=s.begin();
+            if(*it2!=nums[i]) ans2=false;
+        }
+        // s.clear();
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     s.insert(nums[i]);
+        //     auto it=s.begin();
+        //     if(*it!=nums[i]) ans2=false;
+        // }  
+        return ans1||ans2;
+        
+        // int pattern=0;
+        // for(int i=0;i<nums.size()-1;i++)
+        // {
+        //     if(nums[i]<nums[i+1]) //if monotone increasing
+        //     {
+        //         if(pattern==0)
+        //             pattern=1; //set increasing status
+        //         else if(pattern==-1) // if found decreasing status in btw
+        //             return false; 
+        //     }
+        //     else if(nums[i]>nums[i+1]) //if monotone decreasing
+        //     {
+        //         if(pattern==0)
+        //             pattern=-1; //set decreasing status
+        //         else if(pattern==1) //if found increasing status in btw
+        //             return false;
+        //     }
+        // } //nums[i]==nums[i+1] case is always true
+        // return true;
     }
 };
