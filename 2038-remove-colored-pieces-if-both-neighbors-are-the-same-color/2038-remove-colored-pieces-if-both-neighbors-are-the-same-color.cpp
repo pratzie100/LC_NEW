47a3b651a022c8cf08626bb31cf2cf65d9d9c176
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-10-02 09:36:40 +0530

    Time: 148 ms (5.24%), Space: 67.9 MB (5.12%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool winnerOfGame(string s) {
+        int n=s.size();
+        if(n<=2) return false;
+        unordered_multiset<char>bada;
+        unordered_multiset<char>badb;
+        for(int i=1;i<n-1;i++)
+        {
+            if(s[i]==s[i-1] && s[i]==s[i+1])
+            {
+                if(s[i]=='A') bada.insert(s[i]);
+                else badb.insert(s[i]);
+            }
+        }
+        // cout<<bada.size()<<" "<<badb.size();
+        return bada.size()>badb.size();
+    }
+};
