8153bb1959a37af485aad6ff66b9fdedaeb569b1
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-10-02 09:37:50 +0530

    Time: 34 ms (57.13%), Space: 13.7 MB (11.45%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool winnerOfGame(string s) {
+        // int n=s.size();
+        // if(n<=2) return false;
+        // unordered_multiset<char>bada;
+        // unordered_multiset<char>badb;
+        // for(int i=1;i<n-1;i++)
+        // {
+        //     if(s[i]==s[i-1] && s[i]==s[i+1])
+        //     {
+        //         if(s[i]=='A') bada.insert(s[i]);
+        //         else badb.insert(s[i]);
+        //     }
+        // }
+        // // cout<<bada.size()<<" "<<badb.size();
+        // return bada.size()>badb.size();
         int n=s.size();
         if(n<=2) return false;
-        unordered_multiset<char>bada;
-        unordered_multiset<char>badb;
+        int c=0,t=0;
         for(int i=1;i<n-1;i++)
         {
             if(s[i]==s[i-1] && s[i]==s[i+1])
             {
-                if(s[i]=='A') bada.insert(s[i]);
-                else badb.insert(s[i]);
+                if(s[i]=='A') c++;
+                else t++;
             }
         }
         // cout<<bada.size()<<" "<<badb.size();
-        return bada.size()>badb.size();
+        return c>t;
     }
 };
