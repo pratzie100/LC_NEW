ec69a275079b02d5cec42b985a1206895b9ebf59
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-10-10 17:44:58 +0530

    Time: 399 ms (13.81%), Space: 104.3 MB (37.45%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int ans = INT_MAX; 
-        sort(nums.begin(), nums.end()); 
-        nums.erase(unique(begin(nums),end(nums)),end(nums)); 
-        int end=0; // Initialize a variable 'end' to keep track of the end index.
-        for(int l = 0, r = 0; l<nums.size(); l++) 
+        int n=nums.size();
+        //sort(begin(nums),end(nums));
+        set<int>s(begin(nums),end(nums));
+        nums.clear();
+        for(int i:s)//remove duplicates
         {
-            // Loop through the unique elements in the sorted vector.
-            while(r<nums.size() && nums[r]<nums[l]+n) 
-            {
-                // While 'end' is within bounds and the current element plus 'n' is greater
-                // than the element at 'start', continue this loop.
-
-                // Step 1: Increment 'end' by 1 to extend the subarray to the right.
-                r++;
-                // Step 2: Calculate the length of the subarray.
-                int subarrayLength = r - l;
-                // Step 3: Calculate the number of operations needed to maintain consecutive integers.
-                int operationsNeeded = n - subarrayLength;
-                // Update 'ans' with min number of operations found so far.
-                ans = min(ans, operationsNeeded);
-            }
+            nums.push_back(i);
         }
-        return ans; // Return the minimum number of operations.
+        int ans=n;
+        for(int i=0;i<n;i++)
+        {
+            int x=nums[i]+n-1;
+            int ind=upper_bound(begin(nums),end(nums),x)-begin(nums);
+            ans=min(ans,n-ind+i);
+        }
+        return ans;
     }
 };
+
