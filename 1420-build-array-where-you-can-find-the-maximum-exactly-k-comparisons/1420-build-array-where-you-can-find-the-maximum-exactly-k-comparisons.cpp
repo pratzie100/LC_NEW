7014c4a6f36433e051f0848f53d70a0c7b0ac0f8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-10-15 01:08:19 +0530

    Time: 402 ms (5.04%), Space: 12.6 MB (7.10%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int solve(int i, int n, int m, int k,vector<vector<vector<int>>> &dp) 
+    {
+        if(n<=0)
+        {
+            if(k==0) return 1;
+            return 0;
+        }
+        if(dp[i][n][k]!=-1) return dp[i][n][k];
+        int ans = 0;
+        for(int j=1;j<=m;j++)
+        {
+            if(j>i)
+            {
+                if(n>=1 && k>=1)
+                {
+                    ans=(ans+solve(j,n-1,m,k-1,dp))%mod;
+                }
+            }
+            else
+            {
+                if(n>=1)
+                {
+                    ans=(ans+solve(i,n-1,m,k,dp))%mod;
+                }
+            }
+        }
+        return dp[i][n][k] = ans%mod;
+    }
+    int numOfArrays(int n, int m, int k) {
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+        return solve(0,n,m,k,dp);
+    }
+};
