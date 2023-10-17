dc131b4ff76fa4f3a0a3cb1417c7e20a9242bdf9
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-10-18 01:02:57 +0530

    Time: 3 ms (33.98%), Space: 6.2 MB (45.00%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        // int count=0;
-        // while(n>0)
-        // {
-        //     count+=n%2;
-        //     n/=2;    
-        // }
-        // return count;
-        // int count=0;
-        // while(n)
-        // {
-        //     count+=n&1;
-        //     n>>=1;
-        // }
-        // return count;
-        //string s=to_string(n);
-        
-        string s = bitset<32>(n).to_string(); // Convert the unsigned integer to a binary string
-        bitset<32>bit_string(s);
-        return bit_string.count();
+        return __builtin_popcount(n);
     }
 };
