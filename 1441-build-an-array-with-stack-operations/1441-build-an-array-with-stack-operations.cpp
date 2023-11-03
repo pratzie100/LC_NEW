c1133b793679abb886e2b8fe8408715e6f26d851
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-11-03 16:46:31 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (49.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> result;
-        int targetIndex = 0; // Index to track the target array.
-        
-        for (int i = 1; i <= n; ++i) {
-            if (targetIndex == target.size()) {
-                // If we have already matched the target, break out of the loop.
-                break;
+        int c=0;
+        vector<string>ans;
+        for(int i=1;i<=n;i++)
+        {
+            if(c==target.size()) break;
+            if(i==target[c])
+            {
+                c++;
+                ans.push_back("Push");
             }
-            
-            if (i == target[targetIndex]) {
-                // If the current number matches the target, Push it to the stack.
-                result.push_back("Push");
-                targetIndex++;
-            } else {
-                // If the current number doesn't match the target, Push and Pop it.
-                result.push_back("Push");
-                result.push_back("Pop");
+            else
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
             }
         }
-        
-        return result;
+        return ans;
     }
 };
