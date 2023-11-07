101129a13725e65620925710e06bd881c5db01b8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-11-07 07:51:06 +0530

    Time: 134 ms (24.00%), Space: 85.4 MB (42.93%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -9,8 +9,8 @@ public:
         int ans = 0;
         while(!pq.empty()) 
         {
-            if(pq.top()<=ans) 
-                break;            
+            //At each timestep check if the arrival of a monster is less or equal to zero. If thats the case the game is over. Otherwise remove a monster and increment the kill count. The kill count is also equal to minutes played since we kill 1 monster per minute.
+            if(pq.top()-ans<=0) return ans; 
             ans++;
             pq.pop();
         }
