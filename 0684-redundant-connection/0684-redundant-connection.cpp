9be1a9fb090ae3aeb590d73924ada3560be5a0b0
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-11-10 11:12:57 +0530

    Time: 27 ms (6.04%), Space: 19.2 MB (5.75%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,41 +1,37 @@
 class Solution {
-public:
+public:                
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         unordered_map<int,vector<int>>g;
-        vector<int> ans;
-        for(const auto& pair:edges) 
+        for(const auto& pair:edges)
         {
-            // check if there is already a path from pair[0] to pair[1] before adding edge
-            // using dfs
+             //using dfs, check if there is already a path from pair[0] to pair[1] before adding edge
             unordered_set<int>visited;
             stack<int>dfsStack;
             dfsStack.push(pair[0]);
-            while(!dfsStack.empty()) 
+            while(!dfsStack.empty())
             {
-                int current = dfsStack.top();
+                int current=dfsStack.top();
                 dfsStack.pop();
-                if(visited.count(current)== 0) 
+                if(visited.count(current)==0)
                 {
                     visited.insert(current);
-                    for (int neighbor : g[current]) 
+                    for(const auto&x:g[current])
                     {
-                        dfsStack.push(neighbor);
+                        dfsStack.push(x);
                     }
                 }
             }
-            if(visited.count(pair[1]) != 0)  //this is main check 
-            {
-                //(if node pair[1] already visited then adding the current edge would create a cycle because we have found an alternative path from pair[0] to pair[1]. Therefore, we consider this edge as a redundant connection that should be removed to maintain the tree structure.)
-                //ans = pair; 
-                return {pair[0],pair[1]}; 
-            } 
-            else 
+            if(visited.find(pair[1])!=visited.end()) //this is main check
+            {  //if node pair[1] already visited during dfs then adding the current edge would create a cycle because we have found an alternative path from pair[0] to pair[1]. Therefore this edge is a redundant connection that should be removed to maintain the tree structure.
+                return {pair[0],pair[1]};
+                
+            }
+            else
             {
                 g[pair[0]].push_back(pair[1]);
                 g[pair[1]].push_back(pair[0]);
             }
         }
-        return ans; 
-        
+        return {};
     }
 };
