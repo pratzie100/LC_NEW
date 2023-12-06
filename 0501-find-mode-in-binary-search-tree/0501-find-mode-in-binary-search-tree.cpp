4ecdcbe706d6e6b010892b2ae278ec152023028d
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-12-06 09:19:41 +0530

    Time: 10 ms (90.75%), Space: 26 MB (13.48%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,67 +11,32 @@
  */
 class Solution {
 public:
-    int currStreak = 0;
-    int currNum = -1;
-    int maxStreak = 0;
-    vector<int> ans;
-    void inorder(TreeNode *root) 
+    vector<int>ans;
+    void inorder(TreeNode* root)
     {
         if(!root) return;
-        
         inorder(root->left);
-        
-        if(root->val==currNum) currStreak++; 
-        else 
-        {
-            currStreak = 1;
-            currNum = root->val;
-        }
-        if(currStreak==maxStreak) ans.push_back(currNum);
-        else if(currStreak>maxStreak) 
-        {
-            maxStreak=currStreak;
-            ans={currNum};
-        }
-        
+        ans.push_back(root->val);
         inorder(root->right);
     }
-
     vector<int> findMode(TreeNode* root) {
         inorder(root);
-        return ans;
+        unordered_map<int,int>m;
+        for(int i=0;i<ans.size();i++)
+        {
+            m[ans[i]]++;
+        }
+        int maxi=INT_MIN;
+        for(auto i:m)
+        {
+            maxi=max(maxi,i.second);
+        }
+        vector<int>x;
+        for(auto i:m)
+        {
+            if(i.second==maxi)
+                x.push_back(i.first);
+        }
+        return x;
     }
 };
-    // vector<int>v;
-    // void inorder(TreeNode* root)
-    // {
-    //     if(!root) return;
-    //     inorder(root->left);
-    //     v.push_back(root->val);
-    //     inorder(root->right);
-    // }
-    // vector<int> findMode(TreeNode* root) {
-    //     inorder(root);
-    //     map<int,int>m;
-    //     for(int i=0;i<v.size();i++)
-    //     {
-    //         m[v[i]]++;
-    //     }
-    //     int l=INT_MIN;
-    //     for(auto i:m)
-    //     {
-    //         if(i.second>l)
-    //         {
-    //             l=i.second;
-    //         }
-    //     }
-    //     vector<int>ans;
-    //     for(auto i:m)
-    //     {
-    //         if(i.second==l)
-    //         {
-    //             ans.push_back(i.first);
-    //         }
-    //     }
-    //     return ans;
-    // }
