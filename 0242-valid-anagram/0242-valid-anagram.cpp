530ad80ad72012b67dfa531083ee31a009b86193
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-12-16 10:52:06 +0530

    Time: 12 ms (32.61%), Space: 7.6 MB (98.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // unordered_map<string,vector<string>>mp;
-        // string s1=s;
-        // string s2=t;
-        // sort(s.begin(),s.end());
-        // sort(t.begin(),t.end());
-        // mp[s].push_back(s1);
-        // mp[t].push_back(s2);
-        // vector<vector<string>>res;
-        // for(auto i :mp)
-        // {
-        //     vector<string> v= i.second;
-        //     res.push_back(v);
-        // }
-        // if(res.size()==1)
-        //     return true;
-        // return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-            return true;
-        return false;
-
+        sort(begin(s),end(s));
+        sort(begin(t),end(t));
+        return s==t;
+        
     }
 };
