533455effad36ca81af85251c2ada3e554393278
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2023-12-25 11:03:43 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (14.13%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(int i, string s, vector<int> &dp)
+    {
+        if(s[i] == '0')
+            return 0;
+        
+        if(i >= s.length() - 1)
+            return 1;
+        
+        if(dp[i] != -1) 
+            return dp[i];
+        
+        int ans = solve(i + 1, s, dp);
+
+        if(stoi(s.substr(i, 2)) <= 26)
+            ans += solve(i + 2, s, dp);
+        
+        return dp[i] = ans;
+    }
+
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n + 1, -1);
+        return solve(0, s, dp);
+    }
+};
