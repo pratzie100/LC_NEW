4ac8f4f61abcb152cbb72f40dd0ffdbe167122bf
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-01-04 07:27:57 +0530

    Time: 126 ms (44.01%), Space: 85.3 MB (46.04%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -3,23 +3,15 @@ public:
 
     int minOperations(vector<int>& nums) {
         unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]++;
-        }
+        for(int i=0;i<nums.size();i++) m[nums[i]]++;
         int count=0;
         for(auto i:m)
         {
             if(i.second==1) return -1;
-            else
-            {
-                if(i.second%3==0)
-                    count+=i.second/3;
-                else
-                    count+=(i.second/3)+1;
-            }
+            if(i.second%3==0) count+=i.second/3;
+            else count+=(i.second/3)+1;       
         }
         return count;
-        
     }
 };
+//14->(14/3)+1=5 quickest way to reach 14
