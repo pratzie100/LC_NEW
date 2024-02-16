cc0f792d5c274df68273e13a4885ff6cd69ebae8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-02-16 23:47:07 +0530

    Time: 488 ms (5.10%), Space: 78.7 MB (22.02%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -7,7 +7,7 @@ public:
             if(ms.count(a)==ms.count(b)) return a<b;
             return ms.count(a)<ms.count(b);
         });
-        set<int>s(begin(arr)+k,end(arr));
+        unordered_set<int>s(begin(arr)+k,end(arr));
         return s.size();
     }
 };
