e3ec47b55f603d909eef258bc4899b423846e3a7
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-02-19 09:21:16 +0530

    Time: 7 ms (20.09%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<1) 
-            return false;
-        return ((n & n-1) == 0); 
-
-        // if(n==0) return false;
-        //     return (n>0)&&(1073741824%n==0);
+        if(n<=0) return false;
+       // n=abs(n);
+        long long x=n;
+        long long k=0;
+       // 10000
+        while(n!=1)
+        {
+            cout<<n<<endl;
+            n>>=1;
+            k++;
+        }
+        return pow(2,k)==x;
     }
 };
