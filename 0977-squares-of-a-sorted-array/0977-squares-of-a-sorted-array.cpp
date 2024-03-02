e374a98013b7e279775b4bb1e54275daac4e9998
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-02 19:56:15 +0530

    Time: 20 ms (78.12%), Space: 28.4 MB (46.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -9,23 +9,39 @@ public:
         // }
         // sort(begin(ans),end(ans));
         // return ans;
-        deque<int>d;
-        int i=0;
-        int j=nums.size()-1;
-        while(i<j)
+        
+        
+        
+        // deque<int>d;
+        // int i=0;
+        // int j=nums.size()-1;
+        // while(i<j)
+        // {
+        //     if(abs(nums[i])>abs(nums[j])){
+        //         d.push_front(nums[i]*nums[i]);
+        //         i++;
+        //     }
+        //     else{
+        //         d.push_front(nums[j]*nums[j]);
+        //         j--;
+        //     }
+        // }
+        // d.push_front(nums[i]*nums[i]);
+        // // for(int i:d) cout<<i<<" ";
+        // vector<int>ans(begin(d),end(d));
+        // return ans;
+        
+        int n=nums.size();
+        vector<int>ans(n);
+        int l=0;
+        int r=n-1;
+        for(int i=n-1;i>=0;i--)
         {
-            if(abs(nums[i])>abs(nums[j])){
-                d.push_front(nums[i]*nums[i]);
-                i++;
-            }
-            else{
-                d.push_front(nums[j]*nums[j]);
-                j--;
-            }
+            int g=nums[l]*nums[l];
+            int h=nums[r]*nums[r];
+            if(g>h){ans[i]=g;l++;}
+            else{ans[i]=h;r--;}
         }
-        d.push_front(nums[i]*nums[i]);
-        for(int i:d) cout<<i<<" ";
-        vector<int>ans(begin(d),end(d));
         return ans;
     }
 };
