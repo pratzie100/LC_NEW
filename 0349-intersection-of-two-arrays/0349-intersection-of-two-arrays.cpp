ab06cf6506be509a71ec61242ea0b3668c3fb587
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-10 09:40:52 +0530

    Time: 9 ms (25.30%), Space: 14.1 MB (7.77%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        //O(n)
-        // unordered_set<int>s; //this is better than set (nlogn)
-        // unordered_set<int>s_one;
-        // s.insert(begin(nums1),end(nums1));
-        // s_one.insert(begin(nums2),end(nums2));
-        // vector<int>ans;
-        // for(int i:s_one)
-        // {
-        //     if(s.count(i)>0)
-        //         ans.push_back(i);
-        // }
-        // return ans;
-        
-        //two pointer //tc(nlogn for sort then linear) //but actually better for larger inputs
-        sort(begin(nums1),end(nums1));
-        sort(begin(nums2),end(nums2));
-        int m=nums1.size();
-        int n=nums2.size();
-        int i=0,j=0;
+        unordered_set<int>s; //this is better than set (nlogn)
+        unordered_set<int>s_one;
+        s.insert(begin(nums1),end(nums1));
+        s_one.insert(begin(nums2),end(nums2));
         vector<int>ans;
-        while(i<m && j<n)
+        for(int i:s_one)
         {
-            if(nums1[i]==nums2[j])
-            {
-                //to skip duplicates
-                if(find(begin(ans),end(ans),nums1[i])==ans.end()) 
-                    ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-            {
-                i++;
-            }
-            else if(nums2[j]<nums1[i])
-            {
-                j++;
-            }
+            if(s.count(i)>0)
+                ans.push_back(i);
         }
         return ans;
-    }    
+    }
 };
