567defde8ed171c9464afb3fd64eb3865a34684a
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-13 15:30:59 +0530

    Time: 4 ms (35.26%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>pref(n);
-        vector<int>suff(n);
-        pref[0]=1;
-        suff[n-1]=n;
-        for(int i=1;i<n;i++)
+        // vector<int>pref(n);
+        // vector<int>suff(n);
+        // pref[0]=1;
+        // suff[n-1]=n;
+        // for(int i=1;i<n;i++)
+        // {
+        //     pref[i]=pref[i-1]+i+1;
+        // }
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     suff[i]=suff[i+1]+i+1;
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(pref[i]==suff[i])return i+1;
+        // }
+        // return -1;
+        int s=0;
+        for(int i=1;i<=n;i++)s+=i;
+        for(int i=1;i<=n;i++)
         {
-            pref[i]=pref[i-1]+i+1;
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            suff[i]=suff[i+1]+i+1;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(pref[i]==suff[i])return i+1;
-        }
+            int x=(i*(i+1))/2;
+            int y=s-x+i;
+            if(x==y) return i;
+        }   
         return -1;
+        
     }
 };
