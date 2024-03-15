be18629d20adf2f9739a9899a8f5bf3941249cec
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-15 08:18:05 +0530

    Time: 20 ms (30.01%), Space: 28.4 MB (5.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,19 +7,10 @@ public:
         int pre=1,suf=1;
         left[0]=1;
         right[n-1]=1;
-        for(int i=1;i<n;i++)
-        {
-            left[i]=left[i-1]*nums[i-1];
-        }
-        for(int j=n-2;j>=0;j--)
-        {
-            right[j]=right[j+1]*nums[j+1];
-        }
+        for(int i=1;i<n;i++) left[i]=left[i-1]*nums[i-1];
+        for(int j=n-2;j>=0;j--) right[j]=right[j+1]*nums[j+1];
         vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            ans.push_back(left[i]*right[i]);
-        }
+        for(int i=0;i<n;i++)ans.push_back(left[i]*right[i]);
         return ans;
     }
 };
