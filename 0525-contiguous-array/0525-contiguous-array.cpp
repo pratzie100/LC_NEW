579c48996e0684173b084ae253cd6daa3784c111
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-16 23:42:48 +0530

    Time: 85 ms (63.30%), Space: 87.7 MB (74.78%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -5,10 +5,11 @@ public:
         int n=nums.size();
         int c=0;
         int ans=0;
-        m[0]=-1;
+        //m[0]=0;
         for(int i=0;i<n;i++)
         {
             c+=(nums[i]==0)?-1:1;
+            if(c==0) ans=max(ans,i+1);
             if(!m.count(c))m[c]=i;
             else ans=max(ans,i-m[c]);
         }
