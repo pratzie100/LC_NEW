d639a1ae963e80901e09453dbc932475c6513e7e
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-19 19:50:28 +0530

    Time: 64 ms (61.14%), Space: 38 MB (65.52%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,9 +3,10 @@ public:
     int leastInterval(vector<char>& t, int n) {
         int tot=t.size();
         unordered_map<char,int>m;
-        for(char c:t)m[c]++;
         int maxoc=0;
-        for(auto i:m)maxoc=max(maxoc,i.second);
+        for(char c:t){m[c]++;maxoc=max(maxoc,m[c]);}
+        //int maxoc=0;
+        //for(auto i:m)maxoc=max(maxoc,i.second);
         int x=0;
         for(auto i:m){if(i.second==maxoc)x++;}
         return max((maxoc-1)*(n+1)+x,tot);
