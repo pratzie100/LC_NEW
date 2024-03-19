4c4f3e28cb3fcaad553a38a89f43978d0a6b303a
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-19 19:52:22 +0530

    Time: 58 ms (68.63%), Space: 37.9 MB (80.77%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -5,8 +5,6 @@ public:
         unordered_map<char,int>m;
         int maxoc=0;
         for(char c:t){m[c]++;maxoc=max(maxoc,m[c]);}
-        //int maxoc=0;
-        //for(auto i:m)maxoc=max(maxoc,i.second);
         int x=0;
         for(auto i:m){if(i.second==maxoc)x++;}
         return max((maxoc-1)*(n+1)+x,tot);
