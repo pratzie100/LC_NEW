732479500d5c63ff536899a7b0c3bb46f37d77b6
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-24 10:08:43 +0530

    Time: 349 ms (60.00%), Space: 171.1 MB (60.00%) - LeetHub

--- 3092-most-frequent-ids/3092-most-frequent-ids.cpp
+++ 3092-most-frequent-ids/3092-most-frequent-ids.cpp
@@ -7,8 +7,7 @@ public:
         multiset<ll>ms;
         for(int i=0;i<nums.size();i++)
         {
-            ll prev=m[nums[i]];
-            if(prev>0)ms.erase(ms.find(prev));
+            if(m[nums[i]]) ms.erase(ms.find(m[nums[i]]));
             m[nums[i]]+=freq[i];
             if(m[nums[i]]>0) ms.insert(m[nums[i]]);
             if(ms.empty()){ans.push_back(0);continue;}
