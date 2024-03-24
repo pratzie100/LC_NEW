adf8268f7622e03b9fa840c268f3c19ae728a34e
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-24 10:03:27 +0530

    Time: 356 ms (40.00%), Space: 171.1 MB (60.00%) - LeetHub

--- 3092-most-frequent-ids/3092-most-frequent-ids.cpp
+++ 3092-most-frequent-ids/3092-most-frequent-ids.cpp
@@ -15,63 +15,6 @@ public:
             ans.push_back(*ms.rbegin());      
         }
         return ans;
-//         vector<ll>ans;
-//         unordered_map<ll,ll>m;
-//         ll maxoc=INT_MIN;
-//         multiset<ll>ms;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             ll z=m[nums[i]];
-//             m[nums[i]]+=freq[i];
-//             if(m[nums[i]]==0)
-//             {
-//                 // auto it=ms.end();
-//                 // it--;
-//                 // ll x=freq[i]+(*it);
-//                 // if(x==0)
-//                 // {
-//                 //     ms.erase(*it);
-//                 // }
-//                 m.erase(nums[i]);
-//                 ms.erase(z);
-//                 if(m.empty()){ans.push_back(0);continue;}
-                
-//             }
-//             ms.insert(m[nums[i]]);
-//             auto it=ms.end();
-//             it--;
-//              maxoc=max(maxoc,*it);
-//             ans.push_back(maxoc);
-//         }
-//         return ans;
-        
-        
-        // vector<ll>ans;
-        // unordered_map<ll,ll>m;
-        // multiset<ll>ms;
-        // ll maxoc = 0; 
-        // for(int i=0;i<nums.size();i++) 
-        // {
-        //     ll prevFreq=m[nums[i]];
-        //     m[nums[i]]+=freq[i];
-        //     if(prevFreq > 0)
-        //         ms.erase(ms.find(prevFreq));
-        //     if(m[nums[i]] > 0)
-        //         ms.insert(m[nums[i]]);
-        //     if(ms.empty()) {
-        //         ans.push_back(0);
-        //         continue;
-        //     }
-        //     maxoc = *ms.rbegin();
-        //     ans.push_back(maxoc);
-        // }
-        // return ans;
     }
-    // [7,3,3,9,3]
-    // [2,5,5,3,-10]
-    // [2,5,10,10,3]
-//     [2,3,2,5,6]
-// [2,2,-2,2,1]
-    // [2,2,2,2,2]
 };
 
