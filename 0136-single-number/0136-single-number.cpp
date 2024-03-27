21ec1be3710916017b04fab53a6d5a4dcd67d51d
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-03-27 15:35:18 +0530

    Time: 4 ms (98.04%), Space: 19.4 MB (34.28%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // int i;
-        // for(i=0;i<nums.size();i++)
-        // {
-        //     if(count(nums.begin(),nums.end(),nums[i])==1)
-        //         break;
-        // }
-        // return nums[i];
-        map<int,vector<int>>mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]].push_back(i);
-        }
-        for(auto i:mp)
-        {
-            if(i.second.size()==1)
-            {
-                return i.first;
-            }
-        }
-        return -1;
+        int ans=0;
+        for(int i:nums) ans^=i;
+        return ans;
     }
 };
