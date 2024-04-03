717c7ae5610c3bf683fcf74a800d4095ff84c7e7
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-04-03 21:49:07 +0530

    Time: 1023 ms (23.05%), Space: 11.8 MB (18.51%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,40 @@
 class Solution {
-public:
-    bool dfs(int ind,int i,int j,vector<vector<int>>&moves,vector<vector<char>>&board,string&word)
+    bool dfs(int i,int j,int count,vector<vector<char>>& board,string word)
     {
-        if(ind==word.size()) return true;
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[ind]) return false;
-        char originalchar=board[i][j];
-        board[i][j]='#';
-        for(int c=0;c<moves.size();c++)
-        {
-            int newi=i+moves[c][0];
-            int newj=j+moves[c][1];
-            if(dfs(ind+1,newi,newj,moves,board,word))
-                return true;
-        }
-        board[i][j]=originalchar; //backtrack
-        return false;
+        if(word.length()==count) return true;
+        
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[count])
+            return false;
+        
+        
+        
+        char temp = board[i][j];    
+        board[i][j] = ' ';                          
+
+        bool ans = dfs(i-1,j,count+1,board,word) || 
+                   dfs(i+1,j,count+1,board,word) ||
+                   dfs(i,j-1,count+1,board,word) ||
+                   dfs(i,j+1,count+1,board,word);
+        
+        board[i][j] = temp;                  
+        return ans;
     }
+public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        vector<vector<int>>moves{{0,1},{0,-1},{1,0},{-1,0}};
-        for(int i=0;i<m;i++)
+        int n = board.size();                   // rows
+        int m = board[0].size();                // cols
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(board[i][j]==word[0] && dfs(0,i,j,moves,board,word))
+                if(board[i][j]==word[0] && dfs(i,j,0,board,word))  
+                {
                     return true;
+                }
             }
         }
+        
         return false;
     }
 };
