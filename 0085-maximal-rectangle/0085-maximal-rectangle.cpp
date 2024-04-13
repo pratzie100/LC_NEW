18ab48f5246ca1fb62f1b752d34e0aa2ff26a2b8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-04-13 11:36:58 +0530

    Time: 29 ms (76.08%), Space: 16.8 MB (87.72%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
+ if (matrix.empty() || matrix[0].empty())
             return 0;
-        
         int rows = matrix.size();
         int cols = matrix[0].size();
         vector<int> heights(cols, 0);
         int maxArea = 0;
-        
-        for(int i=0;i<rows;i++) 
-        {
-            // Updating heights array based on current row
-            for(int j=0;j<cols;j++) 
-            {
-                if (matrix[i][j]=='1')
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
                     heights[j]++;
                 else
                     heights[j] = 0;
+            } 
+            stack<int> st;
+            int area = 0;
+            for (int j = 0; j <= cols; j++) {
+                while (!st.empty() && (j == cols || heights[j] < heights[st.top()])) {
+                    int height = heights[st.top()];
+                    st.pop();
+                    int width = st.empty() ? j : j - st.top() - 1;
+                    area = max(area, width * height);
+                }
+                st.push(j);
             }
-            // Calculate largest rectangle for current row 
-            maxArea = max(maxArea, largestRectangleArea(heights));
-        }
-        return maxArea;
-    }
-    
-    //apply leetcode 84
-    int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-        int maxArea = 0;
-        int n = heights.size();
-        
-        for (int i=0;i<=n;i++) 
-        {
-            while (!st.empty() && (i==n||heights[i]<heights[st.top()])) 
-            {
-                int height = heights[st.top()];
-                st.pop();
-                int width;
-                if (st.empty())
-                    width=i;
-                else
-                    width=i-st.top()-1;
-                maxArea = max(maxArea, width*height);
-            }
-            st.push(i);
+            maxArea = max(maxArea, area);
         }
         return maxArea;
     }
 };
+
