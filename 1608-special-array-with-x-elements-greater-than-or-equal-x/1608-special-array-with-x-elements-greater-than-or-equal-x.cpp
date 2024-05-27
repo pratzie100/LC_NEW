63ea9147cb3c784850e7bf24d2e6f5e3d6cc8757
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-05-27 08:00:05 +0530

    Time: 1 ms (51.88%), Space: 10.3 MB (19.80%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    int func(int mid,vector<int>&nums)
-    {
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>=mid)
-                count++;
-        }
-        return count;
-    }
     int specialArray(vector<int>& nums) {
-        sort(begin(nums),end(nums));
-        int maxi=*max_element(begin(nums),end(nums));
-        int low=0;
-        int high=maxi;
-        int ans=0;
-        while(low<=high)
+        int ans=-1;
+        int n=nums.size();
+        for(int x=1;x<=n;x++)
         {
-            int mid=low+(high-low)/2;
-            if(func(mid,nums)==mid)
+            int c=0;
+            for(int i=0;i<n;i++)
             {
-                return mid;
+                if(nums[i]>=x) c++;
             }
-            else if(func(mid,nums)<mid) //look for better answer on left as smaller one will have better chance EX [3,5,6,7,8]
-            {
-                high=mid-1;
-            }
-            else
-                low=mid+1;
+            if(c==x) return x;
         }
-        return -1;
+        return ans;
     }
 };
