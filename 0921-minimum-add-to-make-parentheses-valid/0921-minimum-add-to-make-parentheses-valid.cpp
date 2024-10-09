ee52c0eac76e7f7b418c440dd163dc063555c0e8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-10-09 11:01:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (32.94%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int moves=0;
-        int count=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(') count++;
-            else count--;
-            if(count<0) //at this point we got one ')' more than '(' so we need to add '(' before '(' which is one move
-            {
-                moves++;  // ()) ---> ()()
-                count=0; //hence we balanced the string again
-            }
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(st.empty()) st.push(s[i]);
+            else if(st.top()=='(' && s[i]==')') st.pop();
+            else st.push(s[i]);
         }
-        moves+=count; //count will store additional moves of inserting ')' at last in order to balance excess of '('  // (((( ---->(((())))
-        return moves;
+        return  st.size();
     }
 };
