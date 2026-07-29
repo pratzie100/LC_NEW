02a12edfff66dee45f985ebe8e5be0a92738fabc
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-07-29 10:43:35 +0530

    Time: 0 ms (100%), Space: 19.8 MB (24.19%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        auto it=upper_bound(begin(letters),end(letters),target);
+        if(it==end(letters)){
+            return *begin(letters);
+        }
+        return *it;
+    }
+};
