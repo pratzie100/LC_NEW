c8e916e50f636accbf06a9609a1fc0458506fd49
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-08-10 02:05:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public: 
-    // unordered_map<int,int>memo;
-    int prat(int n,unordered_map<int,int>&memo){
-        if(memo.count(n)) return memo[n];
-        if(n==1) return 1;
-        if(n==2) return 2;
-        int ans= prat(n-1,memo) + prat(n-2,memo);
-        memo[n]=ans;
-        return ans;
-    }
+    // // unordered_map<int,int>memo;  (can take global memo too)
+    // int prat(int n,unordered_map<int,int>&memo){
+    //     if(memo.count(n)) return memo[n];
+    //     if(n==1) return 1;
+    //     if(n==2) return 2;
+    //     int ans= prat(n-1,memo) + prat(n-2,memo);
+    //     memo[n]=ans;
+    //     return ans;
+    // }
+
+    // int climbStairs(int n) {
+    //     unordered_map<int,int>memo;
+    //     return prat(n,memo);
+    // }
+
+
 
     int climbStairs(int n) {
-        unordered_map<int,int>memo;
-        return prat(n,memo);
+       vector<int>dp(n+2);
+       dp[1]=1;
+       dp[2]=2;
+       for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+       } 
+       return dp[n];
     }
 };
