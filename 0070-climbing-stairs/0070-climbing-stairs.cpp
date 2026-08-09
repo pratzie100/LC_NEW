8a5fe90f81f87d1603223913b2a402b3a665e3e8
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-08-10 02:00:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public: 
-    unordered_map<int,int>memo;
-    int prat(int n){
+    // unordered_map<int,int>memo;
+    int prat(int n,unordered_map<int,int>&memo){
         if(memo.count(n)) return memo[n];
         if(n==1) return 1;
         if(n==2) return 2;
-        int ans= prat(n-1) + prat(n-2);
+        int ans= prat(n-1,memo) + prat(n-2,memo);
         memo[n]=ans;
         return ans;
     }
 
     int climbStairs(int n) {
-        
-        return prat(n);
+        unordered_map<int,int>memo;
+        return prat(n,memo);
     }
 };
